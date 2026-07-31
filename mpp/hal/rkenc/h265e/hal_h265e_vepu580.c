yisding/mpp

commit 264553f957d8126eb5a821702c5157d3763de130

fix[hal_h264e/h265e]: Fix the top address of bitstream buf
The bitstream output address supports wrap-around from bot to top, but the current
size configuration will cause page fault. Set size to -1 to fix this issue.

Error case:
The output buffer size is set to full buffer size which causes page fault
when hardware tries to wrap around to top address.

Solution:
Set output size register to buffer_size - 1 to enable proper wrap-around.

Signed-off-by: Yandong Lin <yandong.lin@rock-chips.com>
Change-Id: I2508dfba2007b3e0fb5c82bd5060fbf77ee66421

mpp/hal/rkenc/h265e/hal_h265e_vepu580.c
@@ -2936,7 +2936,7 @@ MPP_RET hal_h265e_v580_start(void *hal, HalEncTask *enc_task)
                 reg_base->reg0173_bsbb_addr = mpp_buffer_get_fd(enc_task->output);
 
                 mpp_dev_multi_offset_update(frm->reg_cfg, 175, offset);
-                mpp_dev_multi_offset_update(frm->reg_cfg, 172, mpp_buffer_get_size(enc_task->output));
+                mpp_dev_multi_offset_update(frm->reg_cfg, 172, mpp_buffer_get_size(enc_task->output) - 1);
             } else {
                 reg_base->reg0172_bsbt_addr = mpp_buffer_get_fd(frm->hw_tile_stream[k - 1]);
                 /* TODO: stream size relative with syntax */
@@ -2945,7 +2945,7 @@ MPP_RET hal_h265e_v580_start(void *hal, HalEncTask *enc_task)
                 reg_base->reg0175_adr_bsbs  = reg_base->reg0172_bsbt_addr;
 
                 mpp_dev_multi_offset_update(frm->reg_cfg, 175, 0);
-                mpp_dev_multi_offset_update(frm->reg_cfg, 172, mpp_buffer_get_size(frm->hw_tile_stream[k - 1]));
+                mpp_dev_multi_offset_update(frm->reg_cfg, 172, mpp_buffer_get_size(frm->hw_tile_stream[k - 1]) - 1);
             }
 
             offset = ctx->fbc_header_len;

mpp/hal/rkenc/h264e/hal_h264e_vepu510.c
@@ -1355,7 +1355,7 @@ static void setup_vepu510_io_buf(HalVepu510RegSet *regs, MppDevRegOffCfgs *offse
 
     mpp_dev_multi_offset_update(offsets, 161, off_in[0]);
     mpp_dev_multi_offset_update(offsets, 162, off_in[1]);
-    mpp_dev_multi_offset_update(offsets, 172, siz_out);
+    mpp_dev_multi_offset_update(offsets, 172, siz_out - 1);
     mpp_dev_multi_offset_update(offsets, 174, off_out);
 
     hal_h264e_dbg_func("leave\n");

mpp/hal/rkenc/h264e/hal_h264e_vepu511.c
@@ -1383,7 +1383,7 @@ static void setup_vepu511_io_buf(HalVepu511RegSet *regs, MppDevRegOffCfgs *offse
 
     mpp_dev_multi_offset_update(offsets, 161, off_in[0]);
     mpp_dev_multi_offset_update(offsets, 162, off_in[1]);
-    mpp_dev_multi_offset_update(offsets, 172, siz_out);
+    mpp_dev_multi_offset_update(offsets, 172, siz_out - 1);
     mpp_dev_multi_offset_update(offsets, 174, off_out);
 
     regs->reg_frm.meiw_addr = task->md_info ? mpp_buffer_get_fd(task->md_info) : 0;

mpp/hal/rkenc/h264e/hal_h264e_vepu511a.c
@@ -1259,7 +1259,7 @@ static void setup_vepu511a_io_buf(HalVepu511aRegSet *regs, MppDevRegOffCfgs *off
 
     mpp_dev_multi_offset_update(offsets, 161, off_in[0]);
     mpp_dev_multi_offset_update(offsets, 162, off_in[1]);
-    mpp_dev_multi_offset_update(offsets, 172, siz_out);
+    mpp_dev_multi_offset_update(offsets, 172, siz_out - 1);
     mpp_dev_multi_offset_update(offsets, 174, off_out);
 
     reg_frm->common.meiw_addr = task->md_info ? mpp_buffer_get_fd(task->md_info) : 0;

mpp/hal/rkenc/h264e/hal_h264e_vepu540c.c
@@ -1006,7 +1006,7 @@ static void setup_vepu540c_io_buf(HalVepu540cRegSet *regs, MppDev dev,
     regs->reg_base.bsbb_addr  = fd_out;
     regs->reg_base.adr_bsbs   = fd_out;
     regs->reg_base.bsbr_addr  = fd_out;
-    mpp_dev_set_reg_offset(dev, 172, siz_out);
+    mpp_dev_set_reg_offset(dev, 172, siz_out - 1);
     mpp_dev_set_reg_offset(dev, 174, off_out);
 
     regs->reg_base.rfpt_h_addr = 0xffffffff;

mpp/hal/rkenc/h264e/hal_h264e_vepu541.c
@@ -969,7 +969,7 @@ static void setup_vepu541_io_buf(Vepu541H264eRegSet *regs, MppDev dev,
 
     mpp_dev_set_reg_offset(dev, 71, off_in[0]);
     mpp_dev_set_reg_offset(dev, 72, off_in[1]);
-    mpp_dev_set_reg_offset(dev, 83, siz_out);
+    mpp_dev_set_reg_offset(dev, 83, siz_out - 1);
     mpp_dev_set_reg_offset(dev, 86, off_out);
 
     hal_h264e_dbg_func("leave\n");

mpp/hal/rkenc/h264e/hal_h264e_vepu580.c
@@ -1427,7 +1427,7 @@ static void setup_vepu580_io_buf(HalVepu580RegSet *regs, MppDevRegOffCfgs *offse
 
     mpp_dev_multi_offset_update(offsets, 161, off_in[0]);
     mpp_dev_multi_offset_update(offsets, 162, off_in[1]);
-    mpp_dev_multi_offset_update(offsets, 172, siz_out);
+    mpp_dev_multi_offset_update(offsets, 172, siz_out - 1);
     mpp_dev_multi_offset_update(offsets, 175, off_out);
 
     hal_h264e_dbg_func("leave\n");

mpp/hal/rkenc/h265e/hal_h265e_vepu510.c
@@ -1669,7 +1669,7 @@ void vepu510_h265_set_hw_address(H265eV510HalContext *ctx, H265eVepu510Frame *re
     regs->common.adr_rfpb_b = 0;
 
     mpp_dev_multi_offset_update(ctx->reg_cfg, 174, mpp_packet_get_length(task->packet));
-    mpp_dev_multi_offset_update(ctx->reg_cfg, 172, mpp_buffer_get_size(enc_task->output));
+    mpp_dev_multi_offset_update(ctx->reg_cfg, 172, mpp_buffer_get_size(enc_task->output) - 1);
 
     regs->common.pic_ofst.pic_ofst_y = mpp_frame_get_offset_y(task->frame);
     regs->common.pic_ofst.pic_ofst_x = mpp_frame_get_offset_x(task->frame);

commit ad32534571564aae2ee5cca26547c3738e3366ed

fix[hal_h265e]: set the bitstream top address to size - 1 on vepu580
Commit 264553f9 ("fix[hal_h264e/h265e]: Fix the top address of bitstream
buf") changed the reg 172 bitstream-top offset from the full buffer size
to size - 1, because the hardware wraps from bot to top and faults when
the top address is one past the end of the mapping.

That commit updated the two tiled vepu580 H.265 call sites and the H.264
one, but missed the non-tiled H.265 path in vepu580_h265_set_hw_address,
so every ordinary HEVC encode still programs base + size.

On RK3588 this makes the whole HEVC encoder unusable against a driver
that range-checks register offsets against the dma-buf: the offset is
rejected and hal_h265e_v580_start fails for every frame. Against the
vendor driver, which does not bounds-check, it is the page fault the
original commit set out to remove.

Apply the same size - 1 convention to the remaining call site.

Fixes: 264553f9 ("fix[hal_h264e/h265e]: Fix the top address of bitstream buf")
Signed-off-by: Yi Ding <yi.s.ding@gmail.com>

mpp/hal/rkenc/h265e/hal_h265e_vepu580.c
@@ -2545,7 +2545,7 @@ void vepu580_h265_set_hw_address(H265eV580HalContext *ctx, HalEncTask *task)
     regs->reg0175_adr_bsbs  = regs->reg0172_bsbt_addr;
 
     mpp_dev_multi_offset_update(frm->reg_cfg, 175, mpp_packet_get_length(task->packet));
-    mpp_dev_multi_offset_update(frm->reg_cfg, 172, mpp_buffer_get_size(enc_task->output));
+    mpp_dev_multi_offset_update(frm->reg_cfg, 172, mpp_buffer_get_size(enc_task->output) - 1);
 
     regs->reg0204_pic_ofst.pic_ofst_y = mpp_frame_get_offset_y(task->frame);
     regs->reg0204_pic_ofst.pic_ofst_x = mpp_frame_get_offset_x(task->frame);